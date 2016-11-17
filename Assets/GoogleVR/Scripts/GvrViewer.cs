44913570ae21d2524e68f0cdcd42a396f71a91aa
Eldaste <oliver.m.wimmer@vanderbilt.edu>
2016-11-16 18:27:47 -0600

    Revreing commentout

--- Assets/GoogleVR/Scripts/GvrViewer.cs
+++ Assets/GoogleVR/Scripts/GvrViewer.cs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/*
+
 using UnityEngine;
 using System;
 using System.Collections.Generic;
@@ -542,4 +542,3 @@ public class GvrViewer : MonoBehaviour {
     }
   }
 }
-*/
