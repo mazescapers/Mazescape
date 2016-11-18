6b21aba1e8885e94001d0331c90121a5f6c1a555
Andrew <andrew.s.ragan@vanderbilt.edu>
2016-11-17 20:35:58 -0600

    Added pause boolean

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,29 +6,32 @@ using System;
 public class PlayerController : NetworkBehaviour
 {
     public GameObject beaconPrefab;
+    public static bool paused = false;
 
     // Update is called once per frame
     void Update () {
-
-        if (!isLocalPlayer)
+        if(!paused)
         {
-            return;
-        }
+            if (!isLocalPlayer)
+            {
+                return;
+            }
 
-		GameObject camera = GameObject.Find ("Camera");
-		camera.transform.position = transform.position;
-		camera.transform.rotation = transform.rotation;
-        // Need be updated to work on Androod 
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+            GameObject camera = GameObject.Find("Camera");
+            camera.transform.position = transform.position;
+            camera.transform.rotation = transform.rotation;
+            // Need be updated to work on Androod 
+            float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            CmdPlaceBeacon();
-        }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CmdPlaceBeacon();
+            }
+        }   
     }
 
 	void Start() {
