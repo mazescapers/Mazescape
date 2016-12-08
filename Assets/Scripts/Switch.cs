44cce7fff8c717c25aa364aa875855035b823096
Adam King <adam.c.king@vanderbilt.edu>
2016-12-07 21:38:03 -0600

    Switch Changes

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Switch : MonoBehaviour {
 	private bool pressed = false;
+	public bool sticky = true;
 	public Activatable connection;
 	// Use this for initialization
 	void Start () {
@@ -14,13 +15,25 @@ public class Switch : MonoBehaviour {
 	
 	}
 
+	public void StepOn() {
+		Press ();
+	}
+
+	public void StepOff() {
+		if (!sticky) {
+			Depress ();
+		}
+	}
+
 	public void Press() {
 		pressed = true;
+		this.GetComponent<Renderer>().material.color = Color.yellow;
 		connection.PositiveSignal ();
 	}
 
 	public void Depress() {
 		pressed = false;
+		this.GetComponent<Renderer>().material.color = Color.white;
 		connection.NegativeSignal ();
 	}
 }
